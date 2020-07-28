c5b173dac48e8dbcdae129c3eb46d879fc7a445b
Traveller <51471579+TravellerXi@users.noreply.github.com>
2020-07-28 11:52:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install python3 wget -y
 wget -P /tmp/ https://mirror.fastspeedgo.xyz/V2rayWithWebControl/setup.py
-pip3 install flask pymysql
+pip3 install flask pymysql requests
 /usr/bin/python3 /tmp/setup.py
